9376b5c26ba83ba25f97ff0e2429296fb9bfd81a
Ivan <ivan@athena.ndnxr.com>
2014-05-08 04:59:16 -0400

    Initial commit.

---
+++ tagteam/tagteam/settings.py
@@ -0,0 +1,99 @@
+"""
+Django settings for tagteam project.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.6/topics/settings/
+
+For the full list of settings and their values, see
+https://docs.djangoproject.com/en/1.6/ref/settings/
+"""
+
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import os
+BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+
+
+# Quick-start development settings - unsuitable for production
+# See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'w04)kly&t4m6$r_(wko+^uppipl93q^3dmv$jqhewzhjutzstp'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+#DEBUG = True
+
+#TEMPLATE_DEBUG = True
+DEBUG = True # False
+
+TEMPLATE_DEBUG = True # False
+
+ALLOWED_HOSTS = ['athena.ndnxr.com']
+
+
+# Application definition
+
+INSTALLED_APPS = (
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'tagteam',
+    'post'
+)
+
+MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+ROOT_URLCONF = 'tagteam.urls'
+
+WSGI_APPLICATION = 'tagteam.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/1.6/ref/settings/#databases
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'Athena',
+        'USER': 'athena',
+        'PASSWORD': 'athena',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
+    }
+}
+'''
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+'''
+
+# Internationalization
+# https://docs.djangoproject.com/en/1.6/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.6/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = '/home/ivan/thor/tagteam/tagteam/static/'
---
+++ tagteam/tagteam/urls.py
@@ -0,0 +1,14 @@
+from django.conf.urls import patterns, include, url
+
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'tagteam.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^$', 'tagteam.views.index', name='index'),
+    url(r'^post/', include('post.urls'), name='post'),
+    url(r'^admin/', include(admin.site.urls)),
+)
