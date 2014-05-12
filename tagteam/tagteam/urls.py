04ab2798f94b3e40acd3e2041315190a3569210c
Ivan <ivanyeo@cs.ucla.edu>
2014-05-11 22:45:13 -0400

    Update urls.py for query processing.

--- tagteam/tagteam/urls.py
+++ tagteam/tagteam/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^$', 'tagteam.views.index', name='index'),
+    url(r'^query/', include('query.urls', namespace='query'), name='query'),
     url(r'^post/', include('post.urls'), name='post'),
     url(r'^admin/', include(admin.site.urls)),
 )
